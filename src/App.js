6007dfcaaefcaf10a1f40b88e10ecfd2e5d33e1b
Efi Gordon <efigordon90@gmail.com>
2020-04-19 12:05:35 +0300

    add tests and install enzyme

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-//import Checkout from './containers/Checkout/Checkout';
-//import Orders from './containers/Orders/Orders';
-//import Auth from './containers/Auth/Auth';
+
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions';
 
---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+// React recognize automaticly files with .test extentsins and react adds it to the tests.
+
+configure({
+    adapter: new Adapter()
+});
+
+
+describe('This is description of the test. <NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    })
+
+    it('should render 2 <NavigationItem /> elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems />)
+        expect(wrapper.find(NavigationItem))
+            .toHaveLength(2);
+    });
+
+    it('should render 3 <NavigationItem /> elements if  authenticated', () => {
+        //const wrapper = shallow(<NavigationItems isAuthenticated />)
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.find(NavigationItem))
+            .toHaveLength(3);
+    });
+})
+
